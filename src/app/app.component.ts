3354ffe77db4f8e375bbc663d48683371cb26d3e
Adam Ehven <aehven@binarytrees.biz>
2019-07-03 16:01:21 -0600

    menu closes itself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,18 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuController: MenuController
   ) {
     this.initializeApp();
   }
@@ -35,5 +24,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  menuClicked() {
+    this.menuController.close();
   }
 }
