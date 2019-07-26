ef62232b3fa0570dbf0a0e644bbe47c7b368939c
Adam Ehven <aehven@binarytrees.biz>
2019-07-25 18:11:37 -0600

    notification job

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiPath: `http://192.168.2.10:3030`,
+  apiPath: `http://localhost:3030`,
   demoUser: "a@null.com",
   demoPassword: "password",
-  webSocketPath: `ws://192.168.2.10:3030/cable`,
+  webSocketPath: `ws://localhost:3030/cable`,
   homePath: "/home",
   allowAnonymousUsers: false,
   usersBelongToCustomers: true,
