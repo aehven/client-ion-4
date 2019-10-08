3950ba25ad2416d015eb61a3ad6549b79016b0af
Adam Ehven <aehven@binarytrees.biz>
2019-10-08 14:36:35 -0600

    tweak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiPath: `${window.location.protocol}//${window.location.host}`,
+  demoUser: "a@null.com",
+  demoPassword: "password",
+  webSocketPath: `${window.location.protocol}//${window.location.host}/cable`,
+  homePath: "/home",
+  allowAnonymousUsers: false,
+  usersBelongToCustomers: false,
+  version: "0.0.0"
 };
