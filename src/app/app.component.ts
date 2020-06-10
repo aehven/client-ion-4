193479445774d0814ed2f93cd71c3184ceeb3da8
Adam Ehven <aehven@binarytrees.biz>
2020-06-10 10:24:02 -0600

    automatic dark mode detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,23 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.setDarkMode();
     });
+  }
+
+  setDarkMode() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) => this.toggleDarkTheme(mediaQuery.matches));
+
+    // Add or remove the "dark" class based on if the media query matches
+  }
+
+  toggleDarkTheme(shouldAdd) {
+    document.body.classList.toggle('dark', shouldAdd);    
   }
 }
