96b4b264bd8512d981d69d2a1641c63f1c9e1c4f
Adam Ehven <aehven@binarytrees.biz>
2020-07-03 11:08:56 -0600

    gql organizations

--- src/app/organization/organization.module.ts
+++ src/app/organization/organization.module.ts
@@ -1,7 +1,13 @@
+import { environment } from '../../environments/environment';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 import { IonicModule } from '@ionic/angular';
 
@@ -24,6 +30,20 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     SharedModule
   ],
-  declarations: [OrganizationPage]
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: environment.graphqlUri
+          })
+        }
+      },
+      deps: [HttpLink]
+    }],
+  declarations: [OrganizationPage],
+  exports: [ApolloModule, HttpLinkModule]
 })
 export class OrganizationPageModule {}
